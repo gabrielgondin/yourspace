796fb1988636364487c2a1614d3723057f6908e7
Gabriel Oliveira <gabrielgondino@gmail.com>
2020-01-25 16:38:05 +0000

    initial commit - create project

---
+++ src/main/kotlin/com/ggo/yourspace/YourspaceApplication.kt
@@ -0,0 +1,11 @@
+package com.ggo.yourspace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YourspaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<YourspaceApplication>(*args)
+}
